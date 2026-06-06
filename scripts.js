860f9c16fdecb9d0e5ad43d8639c0d8449819394
letotecdev <letotec.dev@gmail.com>
2026-06-06 17:03:07 -0300

    versão_base

---
+++ scripts.js
@@ -0,0 +1,65 @@
+
+
+// vai no html e escute a um evento que seria o da pagina carregar  (DOMcontenteLoaded)
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    const artistsData = [
+        { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpg'},
+        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg'},
+        { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg'},
+        { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpg'},
+        { name: 'Luan Santana', image: './img/artista-luan-santana.jpg'},
+        { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpg'}
+    ];
+
+        const albumsData = [
+        { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'SleepyJohn', image: './img/album-white-noise.jpg' },
+        { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano',image: './img/album-ceu-explica.jpg' },
+        { name: 'Nada como um dia após o outro', artist: 'Racionais',image: './img/album-racionais.jpg' },
+        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image:'./img/album-hit-me.jpg' },
+        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpg' },
+        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image:'./img/album-escandalo.jpg' }
+    ];
+
+const artistGrid = document.querySelector('.artists-grid')
+const albumsGrid = document.querySelector('.albums-grid')
+
+
+artistsData.forEach( artist => {
+    const artistCard = document.createElement('div')
+    artistCard.classList.add('artist-card')
+
+    
+    artistCard.innerHTML = `
+        <img src ="${artist.image}" alt ="imagem do ${artist.name}">
+         <div> 
+            <h3>${artist.name}</h3>
+            <p>Artista</p>
+        </div>
+    `
+    // a div foi criada para separar imagem do texto e assim poder alinhar todos os nomes independente do tamanho deles
+
+    artistGrid.appendChild(artistCard)
+})
+
+albumsData.forEach(album => {
+    const albumsCard = document.createElement('div')
+    albumsCard.classList.add('albums-card')
+
+    albumsCard.innerHTML = `
+        <img src ="${album.image}" alt ="imagem do ${album.name}">
+        <div>
+            <h3>${album.name}</h3>
+            <p>${album.artist}</p>
+        </div>
+    `
+    albumsGrid.appendChild(albumsCard)
+})
+
+
+
+})
+
+
+
